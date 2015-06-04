2d05381cc6faca6542487d66f43e8b6167cdf03d
johnhur <johnjhur@gmail.com>
2015-06-03 17:46:17 -0700

    one path edit

--- urlCalc.js
+++ urlCalc.js
@@ -5,21 +5,47 @@ var app = express();
 app.set("view engine", "ejs");
 
 //calcultor operators..
-app.get("/add/:num1/:num2", function(req, res){
-	var sum = parseInt(req.params.num1) + parseInt(req.params.num2)
-	res.render("math", {number:sum});
-})
-app.get("/subtract/:num1/:num2", function(req, res){
-	var difference = parseInt(req.params.num1) - parseInt(req.params.num2)
-	res.render("math", {number:difference});
-})
-app.get("/multiply/:num1/:num2", function(req, res){
-	var result = parseInt(req.params.num1) * parseInt(req.params.num2)
-	res.render("math", {number:result});
-})
-app.get("/divide/:num1/:num2", function(req, res){
-	var quotient = parseInt(req.params.num1) / parseInt(req.params.num2)
-	res.render("math", {number:quotient});
+// app.get("/add/:num1/:num2", function(req, res){
+// 	var sum = Number(req.params.num1) + Number(req.params.num2)
+// 	res.render("math", {number:sum});
+// })
+// app.get("/subtract/:num1/:num2", function(req, res){
+// 	var difference = Number(req.params.num1) - Number(req.params.num2)
+// 	res.render("math", {number:difference});
+// })
+// app.get("/multiply/:num1/:num2", function(req, res){
+// 	var result = Number(req.params.num1) * Number(req.params.num2)
+// 	res.render("math", {number:result});
+// })
+// app.get("/divide/:num1/:num2", function(req, res){
+// 	var quotient = Number(req.params.num1) / Number(req.params.num2)
+// 	res.render("math", {number:quotient});
+// })z
+
+//refactored into one path!!!! BOOOOM:
+
+app.get("/:x/:num1/:num2", function(req, res){
+	var sum = Number(req.params.num1) + Number(req.params.num2);
+	var difference = Number(req.params.num1) - Number(req.params.num2);
+	var result = Number(req.params.num1) * Number(req.params.num2);
+	var quotient = Number(req.params.num1) / Number(req.params.num2);
+	var x = req.params.x;
+
+	if (x === "add") {
+		res.render("math", {number:sum});
+	}
+	else if (x === "subtract") {
+		res.render("math", {number:difference});
+	}
+	else if (x === "multiply") {
+		res.render("math", {number:result});
+	}
+	else if (x === "divide") {
+		res.render("math", {number:quotient});
+	}
+
+
+
 })
 
 //sets server to be hosted at the localhost:3000.. 
